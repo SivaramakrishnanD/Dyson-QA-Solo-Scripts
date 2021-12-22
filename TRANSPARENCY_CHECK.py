4b9a2051e63185a80162e73ae3792a43eafc1541
SivaramakrishnanD <96469951+SivaramakrishnanD@users.noreply.github.com>
2021-12-22 17:38:57 +0530

    Add files via upload

--- TRANSPARENCY_CHECK.py
+++ TRANSPARENCY_CHECK.py
@@ -15,13 +15,16 @@ def transparency_check(MASTERPATH, SLAVEPATH):
     for IMGPATH in [MASTERPATH, SLAVEPATH]:
         IMG       = cv2.imread(IMGPATH, cv2.IMREAD_COLOR)
         boxthresh = 8
+        ssthresh  = 0.95
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/sivaramakrishnand/Desktop/TESTS/GOOGLE VISION TEST/cred.json"
-        client = vision.ImageAnnotatorClient()
+
         with io.open(IMGPATH, 'rb') as imagefile:
             imagedata = imagefile.read()
-        image = vision.Image(content=imagedata)
+
+        client   = vision.ImageAnnotatorClient()
+        image    = vision.Image(content=imagedata)
         response = client.text_detection(image=image)
-        texts = response.text_annotations
+        texts    = response.text_annotations
         for index, item in enumerate(texts):
             if index != 0:
                 coordinates = []
@@ -30,17 +33,15 @@ def transparency_check(MASTERPATH, SLAVEPATH):
                 coordinates[0] = (coordinates[0][0] - boxthresh, coordinates[0][1] - boxthresh)
                 coordinates[2] = (coordinates[2][0] + boxthresh, coordinates[2][1] + boxthresh)
                 cv2.rectangle(IMG, coordinates[0], coordinates[2], (0, 0, 0), -1)
+        IMAGES.append(IMG)
 
-        RES = cv2.cvtColor(IMG, cv2.COLOR_BGR2GRAY)
-        IMAGES.append(RES)
+    MSE  = mse(IMAGES[0], IMAGES[1])
+    SSIM = structural_similarity(IMAGES[0], IMAGES[1], multichannel=True)
 
-    m = mse(IMAGES[0], IMAGES[1])
-    s = structural_similarity(IMAGES[0], IMAGES[1])
-
-    if s < 0.95:
-        print("Structural Similarity: {ssm:.2f} %".format(ssm=s * 100))
-        fig = plt.figure(MASTERPATH.replace("master", ""), [10, 8])
-        plt.suptitle("Structural Similarity: {ssm:.2f} %".format(ssm=s * 100))
+    if SSIM < ssthresh:
+        print("Structural Similarity: {ssm:.2f} %".format(ssm=SSIM * 100))
+        fig = plt.figure(MASTERPATH, [10, 8])
+        plt.suptitle("Structural Similarity: {ssm:.2f} %".format(ssm=SSIM * 100))
         ax = fig.add_subplot(1, 2, 1)
         plt.imshow(IMAGES[0], cmap=plt.cm.gray)
         plt.axis("off")
@@ -50,7 +51,7 @@ def transparency_check(MASTERPATH, SLAVEPATH):
         plt.show()
     else:
         print("No Transparency issues found")
-        print("Structural Similarity: {ssm:.2f} %".format(ssm=s * 100))
+        print("Structural Similarity: {ssm:.2f} %".format(ssm=SSIM * 100))
 
 class App(QWidget):
     def __init__(self):
@@ -58,31 +59,57 @@ class App(QWidget):
         self.setWindowTitle('Select the required PNG/JPEG Files')
         self.setGeometry(10, 10, 640, 480)
 
-        options  = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        files, _ = QFileDialog.getOpenFileNames(self, "Select Image Files", "", "Image Files (*PNG, *JPG);;", options=options)
-        if files:
-            mfiles = []
-            sfiles = []
-            for filepath in files:
-                if re.search("master", filepath):
-                    mfiles.append(filepath)
-                else:sfiles.append(filepath)
+        # options  = QFileDialog.Options()
+        # options |= QFileDialog.DontUseNativeDialog
+        # files, _ = QFileDialog.getOpenFileNames(self, "Select Image Files", "", "Image Files (*PNG, *JPG);;", options=options)
+        # if files:
+        #     mfiles = []
+        #     sfiles = []
+        #     for filepath in files:
+        #         if re.search("master", filepath):
+        #             mfiles.append(filepath)
+        #         else:sfiles.append(filepath)
+        #
+        #     files = []
+        #     for mfile in mfiles:
+        #         file1 = mfile.replace("master", "")
+        #         for sfile in sfiles:
+        #             file2 = sfile.replace("slave", "")
+        #             if file1 == file2:
+        #                 files.append([mfile, sfile])
+        #
+        #     for filepaths in files:
+        #         if len(filepaths) == 2:
+        #             print("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+        #             print("Reading Master File:", filepaths[0], "\nReading Slave File:", filepaths[1])
+        #             print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n")
+        #             transparency_check(filepaths[0], filepaths[1])
 
-            files = []
-            for mfile in mfiles:
-                file1 = mfile.replace("master", "")
-                for sfile in sfiles:
-                    file2 = sfile.replace("slave", "")
-                    if file1 == file2:
-                        files.append([mfile, sfile])
+        mfiles = []
+        sfiles = []
+        for type in ["Master", "Slave"]:
+            options  = QFileDialog.Options()
+            options |= QFileDialog.DontUseNativeDialog
+            files, _ = QFileDialog.getOpenFileNames(self, "Select " + type + " Files", "", "Image Files (*PNG, *JPG);;", options=options)
+            if type == "Master":
+                mfiles = files
+            else:
+                sfiles = files
 
-            for filepaths in files:
-                if len(filepaths) == 2:
-                    print("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-                    print("Reading Master File:", filepaths[0], "\nReading Slave File:", filepaths[1])
-                    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n")
-                    transparency_check(filepaths[0], filepaths[1])
+        files = []
+        for mfile in mfiles:
+            file1 = mfile.split("/")[-1]
+            for sfile in sfiles:
+                file2 = sfile.split("/")[-1]
+                if file1 == file2:
+                    files.append([mfile, sfile])
+
+        for filepaths in files:
+            if len(filepaths) == 2:
+                print("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+                print("Reading Master File:", filepaths[0], "\nReading Slave File:", filepaths[1])
+                print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n")
+                transparency_check(filepaths[0], filepaths[1])
         sys.exit()
 
 if __name__ == '__main__':
